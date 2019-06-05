5b35f36ac13ddcb53b1a214d04828631b26bf884
Hiep <quanghiep294@gmail.com>
2019-06-05 17:34:12 +0700

    config db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('DB is ready!')
